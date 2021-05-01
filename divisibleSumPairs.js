958a3a71f527e5d855cbffe802b921000178de6c
Tasneem Burmawala <tasneemhburmawala@gmail.com>
2021-05-01 11:59:43 -0400

    add divisibleSumPairs.js

---
+++ divisibleSumPairs.js
@@ -0,0 +1,18 @@
+/*
+Given an array of integers and a positive integer k, determine the number of (i,j) pairs where i<j and ar[i] + ar[j] is divisible by k.
+*/
+
+const divisibleSumPairs = (ar, k) =>{
+  let pairs = [];
+  for (let i =0; i<ar.length; i++){
+    for (let j=i+1; j<ar.length; j++){
+      if ((ar[i]+ar[j])%k===0){
+        pairs.push([ar[i], ar[j]]);
+      }
+    }
+    
+  }
+  //return pairs;
+  return pairs.length;
+};
+console.log(divisibleSumPairs([1,2,3,4,5,6], 5));
