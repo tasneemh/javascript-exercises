a5c29c9b47c93ec744da9feb2b7f668825322d6f
Tasneem Burmawala <tasneemhburmawala@gmail.com>
2021-05-19 14:25:58 -0400

    optimized divisibleSumPairs

--- diagonalDifference.js
+++ diagonalDifference.js
@@ -5,36 +5,45 @@ For example, the square matrix  is shown below:
 The left-to-right diagonal 1+5+9 = 15. The right to left diagonal = 3+5+9 = 17. Their absolute difference is |15-17| = 2
  */
 
-const diagonalDifference = (arr) =>{
-let firstDiagonalSum = 0;
-    for (let i=0; i<arr.length; i++){
-      for (let j=0; j<arr.length; j++){
-        if (i===j){
-          //1+5+9 = 15
-        firstDiagonalSum+=arr[i][j];
-        }
+const diagonalDifference = (arr) => {
+  let firstDiagonalSum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (i === j) {
+        //1+5+9 = 15
+        firstDiagonalSum += arr[i][j];
       }
     }
-//console.log("firstDiagonalSum: ",firstDiagonalSum);
-let secDiagonalSum = 0; let countForI = 0; let countForJ = arr.length-1;
-  for (let i=0; i<arr.length; i++){
-    for (let j=0; j<arr.length; j++){
-      if (i===countForI && j===countForJ){
+  }
+  //console.log("firstDiagonalSum: ",firstDiagonalSum);
+
+  let secDiagonalSum = 0;
+  let countForI = 0;
+  let countForJ = arr.length - 1;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (i === countForI && j === countForJ) {
         //3+5+9 = 17
-        secDiagonalSum+=arr[i][j];
+        secDiagonalSum += arr[i][j];
         countForI++;
         countForJ--;
-        }
       }
     }
-//console.log("secDiagonalSum: ",secDiagonalSum);
-//to get absolute value
-let diff = firstDiagonalSum - secDiagonalSum;
-  if (diff<0){
-    return diff * (-1);
+  }
+  //console.log("secDiagonalSum: ",secDiagonalSum);
+  //to get absolute value
+  let diff = firstDiagonalSum - secDiagonalSum;
+  if (diff < 0) {
+    return diff * -1;
   } else {
     return diff;
-  }  
+  }
 };
 
-console.log(diagonalDifference([[1,2,3],[4,5,6],[9,8,9]]));
+console.log(
+  diagonalDifference([
+    [1, 2, 3],
+    [4, 5, 6],
+    [9, 8, 9],
+  ])
+);
--- divisibleSumPairs.js
+++ divisibleSumPairs.js
@@ -1,7 +1,20 @@
 /*
 Given an array of integers and a positive integer k, determine the number of (i,j) pairs where i<j and ar[i] + ar[j] is divisible by k.
 */
-
+const divisibleSumPairs = (ar, k) => {
+  const pairs = [];
+  ar.forEach((element, elementIndex) => {
+    ar.map((value, valueIndex) => {
+      if (valueIndex >= elementIndex + 1) {
+        if ((element + value) % k === 0) {
+          pairs.push([elementIndex, valueIndex]);
+        }
+      }
+    });
+  });
+  return pairs.length;
+};
+/*
 const divisibleSumPairs = (ar, k) =>{
   let pairs = [];
   for (let i =0; i<ar.length; i++){
@@ -15,4 +28,5 @@ const divisibleSumPairs = (ar, k) =>{
   //return pairs;
   return pairs.length;
 };
-console.log(divisibleSumPairs([1,2,3,4,5,6], 5));
+*/
+console.log(divisibleSumPairs([1, 2, 3, 4, 5, 6], 5));
