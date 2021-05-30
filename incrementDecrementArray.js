43e32c81f48bb3a5169c9c59652848f70d95dd54
Tasneem Burmawala <tasneemhburmawala@gmail.com>
2021-05-30 17:35:27 -0400

    changes to incrementDecrementArray.js

--- incrementDecrementArray.js
+++ incrementDecrementArray.js
@@ -1,6 +1,25 @@
-// In JavaScript, define a function `foo` which accepts a JavaScript array of integers and returns a new list where elements with an even index are incremented by 1 and elements with an odd index are decremented by 1. Assume 0 is an even number.
+// In JavaScript, define a function `foo` which accepts a JavaScript array of integers and returns a new array where elements with an even index are incremented by 1 and elements with an odd index are decremented by 1. Assume 0 is an even number.
 // e.g. foo([10, 10, 10, 10, 10]) => [11, 9, 11, 9, 11]
+const foo = (arr) => {
+  return arr.map((num, numIndex) => (numIndex % 2 === 0 ? ++num : --num));
+};
+console.log(foo([10, 10, 10, 10, 10]));
+/*
+const foo = (arr) => {
+  const mappedArr = arr.map((num, numIndex) => {
+    if (numIndex % 2 === 0) {
+      return ++num;
+    } else {
+      return --num;
+    }
+    //return numIndex % 2 === 0 ? ++num : --num;
+  });
+  return mappedArr;
+};
+*/
 
+/*
+alternative 
 const foo = (arr) => {
   const newArr = [];
   for (let i = 0; i < arr.length; i++) {
@@ -13,3 +32,4 @@ const foo = (arr) => {
   return newArr;
 };
 console.log(foo([10, 10, 10, 10, 10]));
+*/
